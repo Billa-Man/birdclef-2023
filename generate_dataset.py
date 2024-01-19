47a5203984b0ebce4a6420506bb73dc8f3d204a6
Sohith Bandari <sohith.bandari@gmail.com>
2024-01-19 21:20:27 +0530

    misc changes

--- generate_dataset.py
+++ generate_dataset.py
@@ -2,20 +2,28 @@ import os
 import numpy as np
 import torchaudio
 
-source_dir = "birdclef-2023/train_audio"
-dest_dir = "birdclef-2023/train_waveforms"
 
-dirs = [folder for folder in os.listdir(source_dir) if os.path.isdir(os.path.join(source_dir, folder))]
-os.makedirs(dest_dir, exist_ok=True)
+def process_audio_file(source_dir, dest_dir):
 
-for folder in dirs:
+    dirs = [folder for folder in os.listdir(source_dir) if os.path.isdir(os.path.join(source_dir, folder))]
+    os.makedirs(dest_dir, exist_ok=True)
 
-    os.makedirs(os.path.join(dest_dir, folder), exist_ok=True)
-    audio_files = [audio for audio in os.listdir(os.path.join(source_dir, folder))]
+    for folder in dirs:
 
-    # Convert audio to waveforms and save as a .npy file
-    for audio_file in audio_files:
-        waveform, _ = torchaudio.load(os.path.join(source_dir, folder, audio_file))
-        waveform_np = waveform.numpy()
-        audio_filename = os.path.splitext(os.path.basename(audio_file))[0]
-        np.save(os.path.join(source_dir, folder, audio_filename) + ".npy", waveform_np)
+        os.makedirs(os.path.join(dest_dir, folder), exist_ok=True)
+        audio_files = [audio for audio in os.listdir(os.path.join(source_dir, folder))]
+
+        # Convert audio to waveforms and save as a .npy file
+        for audio_file in audio_files:
+            waveform, _ = torchaudio.load(os.path.join(source_dir, folder, audio_file))
+            waveform_np = waveform.numpy()
+            audio_filename = os.path.splitext(os.path.basename(audio_file))[0]
+            np.save(os.path.join(source_dir, folder, audio_filename) + ".npy", waveform_np)
+
+
+if __name__ == "__main__":
+
+    source_dir = "birdclef-2023/train_audio"
+    dest_dir = "birdclef-2023/train_waveforms"
+
+    process_audio_file(source_dir, dest_dir)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import matplotlib.pyplot as plt
 
 import os
+import time
 from tqdm import tqdm
 
 import torch
@@ -14,51 +15,68 @@ from config import ModelConfig
 from train_eval import train_and_eval
 
 
-# Set device
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+def main():
 
-# Generate Dataset
-exec(open('generate_dataset.py').read())
+    # Set device
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"MAIN: Device set to '{device}'.")
 
-dataset_dir = "birdclef-2023/train_waveforms"
-dirs = [folder for folder in os.listdir(dataset_dir) if os.path.isdir(os.path.join(dataset_dir, folder))]
-waveform_filepaths = []
+    # Generate Dataset
+    exec(open('generate_dataset.py').read())
+    print(f"MAIN: Dataset generated.")
 
-for folder in tqdm(dirs):
-    audio_files = [waveform for waveform in os.listdir(os.path.join(dataset_dir, folder))]
-    for audio in audio_files:
-        waveform_filepaths.append((os.path.join(dataset_dir, folder, audio)))
+    dataset_dir = "birdclef-2023/train_waveforms"
+    dirs = [folder for folder in os.listdir(dataset_dir) if os.path.isdir(os.path.join(dataset_dir, folder))]
+    waveform_filepaths = []
 
-
-# Create dataloader
-batch_size = 32
-audio_dataset = AudioDataset(waveform_filepaths)
-
-val_size = int(0.2 * len(audio_dataset))
-train_size = len(audio_dataset) - val_size
-
-train_dataset, val_dataset = random_split(audio_dataset, [train_size, val_size])
-
-train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
+    for folder in tqdm(dirs):
+        audio_files = [waveform for waveform in os.listdir(os.path.join(dataset_dir, folder))]
+        for audio in audio_files:
+            waveform_filepaths.append((os.path.join(dataset_dir, folder, audio)))
 
 
-# Initialise model
-model_parameters = ModelConfig()
-model = VisionTransformer(9, 1)
-model.to(device)
+    # Create dataloader
+    batch_size = 32
+    audio_dataset = AudioDataset(waveform_filepaths)
 
-criterion = nn.BCELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=model_parameters.learning_rate, weight_decay=model_parameters.weight_decay)
+    val_size = int(0.2 * len(audio_dataset))
+    train_size = len(audio_dataset) - val_size
 
-# Model training & evaluation
-train_loss, val_loss, cMAP_epoch = train_and_eval(model, criterion, optimizer, model_parameters.num_epochs, device, train_dataloader, val_dataloader)
+    train_dataset, val_dataset = random_split(audio_dataset, [train_size, val_size])
 
-# Plot metrics
-x_values = range(model_parameters.num_epochs)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
 
-plt.plot(x_values, train_loss, label='Train Loss')
-plt.plot(x_values, val_loss, label='Validation Loss')
-plt.plot(x_values, cMAP_epoch, label='cMAP per epoch')
-plt.show()
+    print(f"MAIN: DataLoader created.")
 
+    # Initialise model
+    model_parameters = ModelConfig()
+    model = VisionTransformer(9, 1)
+    model.to(device)
+
+    criterion = nn.BCELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=model_parameters.learning_rate, weight_decay=model_parameters.weight_decay)
+
+    print(f"MAIN: Model initialised.")
+
+    # Model training & evaluation
+
+    print(f"MAIN: Model training started.")
+
+    start = time.time
+    train_loss, val_loss, cMAP_epoch = train_and_eval(model, criterion, optimizer, model_parameters.num_epochs, device, train_dataloader, val_dataloader)
+    end = time.time
+
+    print(f"MAIN: Model training finished. Process took {end-start} seconds.")
+
+    # Plot metrics
+    x_values = range(model_parameters.num_epochs)
+
+    plt.plot(x_values, train_loss, label='Train Loss')
+    plt.plot(x_values, val_loss, label='Validation Loss')
+    plt.plot(x_values, cMAP_epoch, label='cMAP per epoch')
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
